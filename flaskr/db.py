22ac458c48c2d6a99736d8589c1686c6bf0fe66e
blackheart1520 <donghuuhieu1520@gmail.com>
2020-10-09 08:44:23 +0700

    Init
    Signed-off-by: blackheart1520 <donghuuhieu1520@gmail.com>

---
+++ flaskr/db.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sqlite3
+
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
